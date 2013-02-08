af35ff21a1f4b5a276c146987b7ae6ee1da663fa
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-08 13:19:27 +0000

    Implement SSL security for mcollective
      Rework mcollective::* classes attributes to be more fine-grained:
        - Rename broker-related attributes as $broker_*
        - Rename security-related attributes as $security_*
    
      Add support for SSL security support in classes and templates
    
      Add mcollective::client::certificate

---
+++ manifests/client/certificate.pp
@@ -0,0 +1,14 @@
+define mcollective::client::certificate (
+  $key_source,
+  $ensure = 'present',
+  $cert_dir = '/etc/mcollective/ssl/clients',
+) {
+  file {
+    "${cert_dir}/${name}-public.pem":
+      ensure => $ensure,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0640',
+      source => $key_source,
+  }
+}
