a00d31c7ce6c239d87a19ea3e3303b4b4222cead
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-19 16:45:29 +0000

    Add documentation to mcollective::actionpolicy defines

--- manifests/actionpolicy.pp
+++ manifests/actionpolicy.pp
@@ -1,3 +1,42 @@
+# Definition: mcollective::actionpolicy
+#
+# Sets up an action policy for an MCollective agent.
+#
+# See http://projects.puppetlabs.com/projects/mcollective-plugins/wiki/AuthorizationActionPolicy
+# for informations on how action policy rules.
+#
+# You should declare mcollective::node before using this.
+# You should also declare an mcollective::actionpolicy::base resource
+# for the agent you wish to add a rule to.
+#
+# Parameters:
+#   ['ensure']         - Whether the policy rule should be present or absent.
+#   ['agent']          - The agent to which to apply the policy rule.
+#   ['rpccaller']      - The rpc caller (e.g. 'cert=foo' or 'uid=1001')
+#   ['auth']           - Auth rule ('allow' or 'deny').
+#                        Defaults is 'allow'.
+#   ['actions']        - Actions managed by this rule, separated by spaces.
+#                        Defaults to '*'.
+#   ['facts']          - Facts filter for which this rule applies.
+#                        Defaults to '*'.
+#   ['classes']        - Classes filter for which this rule applies.
+#                        Defaults to '*'.
+#   ['order']          - Order for concatenation.
+#                        Defaults to 50.
+#   ['policies_dir']   - Where the policy rules are stored.
+#                        Defaults to '/etc/mcollective/policies'.
+#
+# Actions:
+# - Deploys an MCollective Action Policy rule for an agent
+#
+# Sample Usage:
+#   mcollective::actionpolicy { 'Allow puppetd status for cert foo':
+#     ensure         => present,
+#     agent          => 'puppetd',
+#     rpccaller      => 'cert=foo',
+#     actions        => 'status',
+#   }
+#
 define mcollective::actionpolicy (
   $agent,
   $rpccaller,
--- manifests/actionpolicy/base.pp
+++ manifests/actionpolicy/base.pp
@@ -1,3 +1,27 @@
+# Definition: mcollective::actionpolicy::base
+#
+# Sets up a base action policy for an MCollective agent.
+#
+# See http://projects.puppetlabs.com/projects/mcollective-plugins/wiki/AuthorizationActionPolicy
+# for informations on how action policy rules.
+#
+# You should declare mcollective::node before using this.
+#
+# Parameters:
+#   ['ensure']         - Whether the policy rule should be present or absent.
+#   ['policies_dir']   - Where the policy rules are stored.
+#                        Defaults to '/etc/mcollective/policies'.
+#   ['default_policy'] - The default policy to apply.
+#                        Defaults to 'deny'.
+#
+# Actions:
+# - Deploys a base MCollective Action Policy rule for an agent
+#
+# Sample Usage:
+#   mcollective::actionpolicy::base { 'puppetd':
+#     ensure => present,
+#   }
+#
 define mcollective::actionpolicy::base (
   $ensure = 'present',
   $policies_dir = '/etc/mcollective/policies',
