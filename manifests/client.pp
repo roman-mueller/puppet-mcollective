d4e0fcc7460d824562174f30542073a60a7d6cfa
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-20 08:49:45 +0000

    Document mcollective::client

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,3 +1,61 @@
+# Class: mcollective::client
+#
+# This class provides a simple way to deploy an MCollective client.
+# It will install and configure the necessary packages.
+#
+# This module supports generic STOMP and RabbitMQ connectors,
+# with optional SSL support.
+#
+# It supports PSK and SSL as authentication methods.
+#
+# Parameters:
+#   ['broker_host']       - The middleware broker host to use.
+#   ['broker_port']       - The middleware broker port to use.
+#   ['broker_vhost']      - The middleware broker vhost to use.
+#                           Currently only used with RabbitMQ.
+#   ['broker_user']       - The middleware broker user to use.
+#                           If set to false, the user entry will be
+#                           ommited from the configuration file
+#                           (useful if you want to force using
+#                           environment variables instead).
+#   ['broker_password']   - The middleware broker password to use.
+#   ['broker_ssl']        - Whether to use stomp over SSL
+#   ['broker_ssl_cert']   - If using SSL, the path to the SSL public key.
+#                           Defaults to Puppet's public certicate.
+#   ['broker_ssl_key']    - If using SSL, the path to the SSL private key.
+#                           Defaults to Puppet's private certicate.
+#   ['broker_ssl_ca']     - If using SSL, the path to the SSL CA certificate.
+#                           Defaults to Puppet's CA certificate.
+#   ['security_provider'] - The security provider to use.
+#                           Currently supported are 'psk' and 'ssl'.
+#   ['security_secret']   - If PSK is used, the value of the shared password.
+#   ['security_ssl_server_public']  - If SSL is used, the path to the SSL
+#                                     public server key (shared).
+#   ['security_ssl_client_private'] - If SSL is used, the path to the SSL
+#                                     private client key.
+#   ['security_ssl_client_public']  - If SSL is used, the path to the SSL
+#                                     public client key.
+#   ['connector']         - The connector to use. Either 'stomp' or 'rabbitmq'.
+#                           Defaults to 'rabbitmq'.
+#   ['puppetca_cadir']    - Path to the Puppet CA directory.
+#
+# Actions:
+# - Deploys an MCollective client
+#
+# Sample Usage:
+#   class { '::mcollective::client':
+#     broker_host       => 'rabbitmq.example.com',
+#     broker_port       => '61614',
+#     security_provider => 'psk',
+#     security_secret   => 'P@S5w0rD',
+#   }
+#
+#   class { '::mcollective::client':
+#     broker_host                 => 'rabbitmq.example.com',
+#     broker_port                 => '61614',
+#     security_provider           => 'ssl',
+#   }
+#
 class mcollective::client (
   $broker_host,
   $broker_port,
