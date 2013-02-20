f5385596d8ae5b06df7a3c604469dcc74144310b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-20 08:58:39 +0000

    Document mcollective::plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,3 +1,18 @@
+# Definition: mcollective::plugin
+#
+# Sets up an MCollective plugin using packages.
+#
+# Parameters:
+#   ['ensure']         - Whether the plugin should be present or absent.
+#
+# Actions:
+# - Installs an MCollective plugin using packages.
+#
+# Sample Usage:
+#   mcollective::plugin { 'puppetca':
+#     ensure         => present,
+#   }
+#
 define mcollective::plugin (
   $ensure='present'
 ) {
