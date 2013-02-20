d5917dee0c062a215a4041f4a59124c06bd5cb50
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-02-20 12:57:35 +0000

    Allow $rpccaller@$agent titles in mcollective::actionpolicy

--- manifests/actionpolicy.pp
+++ manifests/actionpolicy.pp
@@ -12,15 +12,19 @@
 # Parameters:
 #   ['ensure']         - Whether the policy rule should be present or absent.
 #   ['agent']          - The agent to which to apply the policy rule.
+#                        If unspecified, the resource title will be
+#                        parsed as '$rpccaller@$agent'.
 #   ['rpccaller']      - The rpc caller (e.g. 'cert=foo' or 'uid=1001')
+#                        If unspecified, the resource title will be
+#                        parsed as '$rpccaller@$agent'.
 #   ['auth']           - Auth rule ('allow' or 'deny').
 #                        Defaults is 'allow'.
-#   ['actions']        - Actions managed by this rule, separated by spaces.
-#                        Defaults to '*'.
-#   ['facts']          - Facts filter for which this rule applies.
-#                        Defaults to '*'.
-#   ['classes']        - Classes filter for which this rule applies.
-#                        Defaults to '*'.
+#   ['actions']        - The array of actions managed by this rule.
+#                        Defaults to ['*'].
+#   ['facts']          - An array of facts for which this rule applies.
+#                        Defaults to ['*'].
+#   ['classes']        - An array of classes for which this rule applies.
+#                        Defaults to ['*'].
 #   ['order']          - Order for concatenation.
 #                        Defaults to 50.
 #   ['policies_dir']   - Where the policy rules are stored.
@@ -29,30 +33,56 @@
 # Actions:
 # - Deploys an MCollective Action Policy rule for an agent
 #
+# Requires:
+# - `ripienaar/concat`
+# - `puppetlabs/stdlib`
+#
 # Sample Usage:
+#
 #   mcollective::actionpolicy { 'Allow puppetd status for cert foo':
 #     ensure         => present,
 #     agent          => 'puppetd',
 #     rpccaller      => 'cert=foo',
-#     actions        => 'status',
+#     actions        => ['status', 'runonce'],
+#   }
+#
+#   mcollective::actionpolicy { 'cert=foo@puppetd':
+#     ensure         => present,
 #   }
 #
 define mcollective::actionpolicy (
-  $agent,
-  $rpccaller,
-  $auth = 'allow',
-  $actions = '*',
-  $facts = '*',
-  $classes = '*',
   $ensure = 'present',
+  $agent = '',
+  $rpccaller = '',
+  $auth = 'allow',
+  $actions = ['*'],
+  $facts = ['*'],
+  $classes = ['*'],
   $order = '50',
   $policies_dir = '/etc/mcollective/policies',
 ) {
+  $_rpccaller = $rpccaller ? {
+    ''      => inline_template('<%= @name.split("@")[0] %>'),
+    default => $rpccaller,
+  }
+
+  $_agent = $agent ? {
+    ''      => inline_template('<%= @name.split("@")[1] %>'),
+    default => $agent,
+  }
+
+  validate_re($auth, 'allow|deny')
+  validate_re($_rpccaller, '(uid|cert)=\S+')
+  validate_re($_agent, '\S+')
+  validate_array($actions)
+  validate_array($facts)
+  validate_array($classes)
+
   $fragment_title    = regsubst($name, '/', '_', 'G')
-  concat::fragment { "mcollective.actionpolicy.${agent}.${fragment_title}":
+  concat::fragment { "mcollective.actionpolicy.${_agent}.${fragment_title}":
     ensure  => $ensure,
     order   => $order,
-    target  => "${policies_dir}/${agent}.policy",
-    content => "${auth}\t${rpccaller}\t${actions}\t${facts}\t${classes}\n",
+    target  => "${policies_dir}/${_agent}.policy",
+    content => template("${module_name}/actionpolicy.erb"),
   }
 }
