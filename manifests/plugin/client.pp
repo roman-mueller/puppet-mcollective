55775014c84aba3ed498783aa77bb38440f30e43
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-08-13 10:55:41 +0200

    Add mcollective::plugin::client wrapper

---
+++ manifests/plugin/client.pp
@@ -0,0 +1,30 @@
+# == Definition: mcollective::plugin::client
+#
+# Wrapper around mcollective::plugin to install clients
+#
+# === Parameters
+#
+#   ['ensure']         - Whether the plugin should be present or absent.
+#   ['old_names']      - Whether to use old names
+#                        Defaults to true
+#
+# === Actions
+#
+# - Installs an MCollective client plugin using packages.
+#
+# === Sample Usage
+#
+#   mcollective::plugin::client { 'package':
+#     ensure         => present,
+#   }
+#
+define mcollective::plugin::client (
+  $ensure='present',
+  $old_names = undef,
+) {
+  ::mcollective::plugin { "${name} client":
+    name      => $name,
+    type      => 'client',
+    old_names => $old_names,
+  }
+}
